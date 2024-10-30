befe3284d0bb6e71e740e3c04f58ba53b58d8185
จารุวิทย์ สิงห์โสม <86384340+Jaruwit01@users.noreply.github.com>
2024-10-30 13:39:20 +0700

    up

--- front-end/src/components/Charts/ImpactScoreBarChart.js
+++ front-end/src/components/Charts/ImpactScoreBarChart.js
@@ -1,3 +1,4 @@
+// ImpactScoreBarChart.js
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
